84cbf3cc67f39363a5cc574a6618563eb677a70e
Vityacv <vityacv@gmail.com>
2018-01-26 03:21:53 +0300

    src/memory.h

--- src/stl.h
+++ src/stl.h
@@ -9,12 +9,14 @@ struct wstring {
   wchar_t *data;
   int length;
   int capacity;
+  wchar_t str[16];
 };
 
 struct string {
-  wchar_t *data;
+  char *data;
   int length;
   int capacity;
+  char str[16];
 };
 }  // namespace stl
 #define _STL_H
